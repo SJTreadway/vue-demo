d6b3fa56873ef5766d82a867ddbd6b05d1a0a6e2
Steven Treadway <steven.treadway@fractalindustries.com>
2018-11-09 13:08:19 -0500

    fixes image issues.

--- src/components/Crypto.vue
+++ src/components/Crypto.vue
@@ -28,13 +28,13 @@
             <td><img :src="getCoinImage(coin.symbol)" alt="">{{ coin.name }}</td>
             <td>{{ coin.symbol }}</td>
             <td>{{ coin.price_usd | currency }}</td>
-            <td v-bind:style="getColor(coin.percent_change_1h)">
+            <td :style="getColor(coin.percent_change_1h)">
               <span v-if="coin.percent_change_1h > 0">+</span>{{ coin.percent_change_1h }}%
             </td>
-            <td v-bind:style="getColor(coin.percent_change_24h)">
+            <td :style="getColor(coin.percent_change_24h)">
               <span v-if="coin.percent_change_24h > 0">+</span>{{ coin.percent_change_24h }}%
             </td>
-            <td v-bind:style="getColor(coin.percent_change_7d)">
+            <td :style="getColor(coin.percent_change_7d)">
               <span v-if="coin.percent_change_7d > 0">+</span>{{ coin.percent_change_7d }}%
             </td>
             <td>{{ coin.market_cap_usd | currency }}</td>
@@ -48,18 +48,18 @@
 <script>
 import axios from 'axios'
 // The API we're using for grabbing metadata about each cryptocurrency
-// (including logo images). The service can be found at:
-// https://www.cryptocompare.com/api/
-let CRYPTOCOMPARE_API_URI = "https://www.cryptocompare.com";
+// The service can be found at: https://www.cryptocompare.com/api/
+const CRYPTOCOMPARE_API_URI = 'https://min-api.cryptocompare.com'
+
+const CRYPTOCOMPARE_IMAGE_API_URI = 'https://www.cryptocompare.com'
 
 // The API we're using for grabbing cryptocurrency prices.  The service can be
 // found at: https://coinmarketcap.com/api/
-let COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
+const COINMARKETCAP_API_URI = 'https://api.coinmarketcap.com'
 
 // The amount of milliseconds (ms) after which we should update our currency
 // charts.
-let UPDATE_INTERVAL = 60 * 1000;
-
+const UPDATE_INTERVAL = 60 * 1000
 
 export default {
   name: 'crypto',
@@ -78,56 +78,52 @@ export default {
      * Load up all cryptocurrency data.  This data is used to find what logos
      * each currency has, so we can display things in a friendly way.
     */
-   getCoinData: function() {
-     let self = this
-
-      axios.get(CRYPTOCOMPARE_API_URI + '/api/data/coinlist')
-      .then(resp => {
-        this.coinData = resp.data.Data
-        this.getCoins()
-      })
-      .catch(error => {
-        this.getCoins()
-        console.error('error receiving coin data: ', error)
-      })
+    getCoinData: function () {
+      axios.get(CRYPTOCOMPARE_API_URI + '/data/all/coinlist')
+        .then(resp => {
+          this.coinData = resp.data.Data
+          this.getCoins()
+        })
+        .catch(error => {
+          this.getCoins()
+          console.error('error receiving coin data: ', error)
+        })
     },
 
     /**
      * Get the top 10 cryptocurrencies by value.  This data is refreshed each 5
      * minutes by the backing API service.
     */
-   getCoins: function() {
-     let self = this
-
+    getCoins: function () {
       axios.get(COINMARKETCAP_API_URI + '/v1/ticker/?limit=10')
-      .then(resp => {
-        this.coins = resp.data
-      })
-      .catch(err => console.error("error retriving coins: ", err))
+        .then(resp => {
+          this.coins = resp.data
+        })
+        .catch(err => console.error('error retriving coins: ', err))
     },
 
     /**
      * Given a cryptocurrency ticket symbol, return the currency's logo
      * image.
     */
-   getCoinImage: function(symbol) {
-      return CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl
+    getCoinImage: function (symbol) {
+      return CRYPTOCOMPARE_IMAGE_API_URI + this.coinData[symbol].ImageUrl
     },
     /**
      * Return a CSS color (either red or green) depending on whether or
      * not the value passed in is negative or positive.
     */
-   getColor: (num) => {
-     return num > 0 ? "color:green" : "color:red";
+    getColor: function (num) {
+      return num > 0 ? 'color: green' : 'color: red'
     },
     /**
      * Once the page has been loaded and all of our app stuff is working, we'll
      * start polling for new cryptocurrency data every minute.
      */
-    refresh: () => {
+    refresh: function () {
       setInterval(() => {
-      this.getCoins()
-    }, UPDATE_INTERVAL)
+        this.getCoins()
+      }, UPDATE_INTERVAL)
     }
   }
 }
